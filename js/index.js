77d39faf2e27376eaf1511c33198bdaeb70575cb
GONGTING520 <954160311@qq.com>
2018-01-28 23:11:32 +0800

    不给遮罩层绑定点击事件，而是给遮罩层下面的三个div绑定点击事件

--- js/index.js
+++ js/index.js
@@ -13,36 +13,40 @@ $(function () {
     });
 
     // 给遮罩层绑定鼠标事件
-    $mask.on('mousedown', function (e) {
+    $('.left', $mask).on('click', function () {
+        // 点击左侧区域        
+        $imgCarouselUl.iNow--;
+        $imgCarouselUl.iNow = $imgCarouselUl.iNow < 0 ? $imgCarouselLis.length - 1 : $imgCarouselUl.iNow;
+        changeImg();
+    });
+
+    $('.right', $mask).on('click', function () {
+        // 点击右侧区域
+        $imgCarouselUl.iNow++;
+        $imgCarouselUl.iNow = $imgCarouselUl.iNow == $imgCarouselLis.length ? 0 : $imgCarouselUl.iNow;
+        changeImg();
+    });
+
+    $('.center', $mask).on('mousedown', function (e) {
         let iLeft = e.clientX;
         let iUlLeft = $imgCarouselUl.offset().left;
         let bFlag = false; //表示鼠标移动时是否切换图片索引
-        // 点击左侧区域
-        if (iLeft < $imgCarouselUl.iOneLiWidth / 3) {
-            $imgCarouselUl.iNow--;
-            $imgCarouselUl.iNow = $imgCarouselUl.iNow < 0 ? $imgCarouselLis.length - 1 : $imgCarouselUl.iNow;
-        } else if (iLeft > $imgCarouselUl.iOneLiWidth * 2 / 3) {
-            // 点击右侧区域
-            $imgCarouselUl.iNow++;
-            $imgCarouselUl.iNow = $imgCarouselUl.iNow == $imgCarouselLis.length ? 0 : $imgCarouselUl.iNow;
-        } else {
-            // 点击中间区域
-            $mask.get(0).onmousemove = function (e) {
-                let iNowLeft = e.clientX;
-                $imgCarouselUl.animate({
-                    left: iUlLeft + iNowLeft - iLeft
-                }, 0);
-                if (!bFlag && iNowLeft - iLeft >= 100) {
-                    bFlag = true;
-                    $imgCarouselUl.iNow--;
-                    $imgCarouselUl.iNow = $imgCarouselUl.iNow < 0 ? $imgCarouselLis.length - 1 : $imgCarouselUl.iNow;
-                } else if (!bFlag && iNowLeft - iLeft <= -100) {
-                    bFlag = true;
-                    $imgCarouselUl.iNow++;
-                    $imgCarouselUl.iNow = $imgCarouselUl.iNow == $imgCarouselLis.length ? 0 : $imgCarouselUl.iNow;
-                }
-            };
-        }
+        // 点击中间区域
+        $mask.get(0).onmousemove = function (e) {
+            let iNowLeft = e.clientX;
+            $imgCarouselUl.animate({
+                left: iUlLeft + iNowLeft - iLeft
+            }, 0);
+            if (!bFlag && iNowLeft - iLeft >= 100) {
+                bFlag = true;
+                $imgCarouselUl.iNow--;
+                $imgCarouselUl.iNow = $imgCarouselUl.iNow < 0 ? $imgCarouselLis.length - 1 : $imgCarouselUl.iNow;
+            } else if (!bFlag && iNowLeft - iLeft <= -100) {
+                bFlag = true;
+                $imgCarouselUl.iNow++;
+                $imgCarouselUl.iNow = $imgCarouselUl.iNow == $imgCarouselLis.length ? 0 : $imgCarouselUl.iNow;
+            }
+        };
     });
 
     $(document).on('mouseup', function () {
