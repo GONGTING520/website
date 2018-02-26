68b31ba4a3ef1845892f5a330d211ecd956c3c52
GONGTING520 <954160311@qq.com>
2018-02-26 23:34:56 +0800

    添加向上箭头的事件及样式

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,6 +2,9 @@
   <div id="app">
     <slideshow id="slideshow" :imgSrc='imgArr'></slideshow>
     <project-index :bgSrc='bgSrc' :projectInfo='projectInfo'></project-index>
+    <transition tag="div" name="fade">
+      <div class="arrow-up" @click="up" v-show="isShowUp" :style="arrowUpStyle"></div>
+    </transition>
   </div>
 </template>
 
@@ -14,7 +17,11 @@ export default {
     return {
       imgArr: [],
       projectInfo: null,
-      bgSrc: ""
+      bgSrc: "",
+      isShowUp: false,
+      arrowUpStyle: {
+        background: "url(./static/img/grab-up.png) no-repeat center"
+      }
     };
   },
   components: {
@@ -41,6 +48,29 @@ export default {
       .catch(function() {
         console.log("error");
       });
+  },
+  mounted() {
+    $(window).on("scroll", () => {
+      let iScrollTop =
+        document.body.scrollTop || document.documentElement.scrollTop;
+      if (iScrollTop < document.documentElement.clientHeight) {
+        this.isShowUp = false;
+      } else {
+        this.isShowUp = true;
+      }
+    });
+  },
+  methods: {
+    up() {
+      $(document.body)
+        .add(document.documentElement)
+        .animate(
+          {
+            scrollTop: 0
+          },
+          1000
+        );
+    }
   }
 };
 </script>
@@ -65,6 +95,16 @@ li {
   width: 100%;
   height: 100vh;
 }
+.arrow-up {
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  position: fixed;
+  bottom: 65px;
+  right: 5%;
+  animation: vertical 2.5s ease infinite;
+  z-index: 10;
+}
 @keyframes vertical {
   30% {
     bottom: 65px;
@@ -81,5 +121,17 @@ li {
   70% {
     bottom: 65px;
   }
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
 }
 </style>
