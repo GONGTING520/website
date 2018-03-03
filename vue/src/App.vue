eb85a634a750d4da1251e49323600b99e59342a2
GONGTING520 <954160311@qq.com>
2018-03-03 22:01:02 +0800

    引入主页面组件和详情信息组件，并通过组件弹出的事件切换动画

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,76 +1,29 @@
 <template>
-  <div id="app">
-    <slideshow id="slideshow" :imgSrc='imgArr'></slideshow>
-    <project-index :bgSrc='bgSrc' :projectInfo='projectInfo'></project-index>
-    <transition tag="div" name="fade">
-      <div class="arrow-up" @click="up" v-show="isShowUp" :style="arrowUpStyle"></div>
-    </transition>
-  </div>
+  <transition-group tag="div" :name="slide" id="app">
+    <index key="0" v-show="!showInfo" @info="changeSlideDirction('right')"></index>
+    <info key="1" v-show="showInfo" @home="changeSlideDirction('left')"></info>
+  </transition-group>
 </template>
 
 <script>
-import axios from "axios";
-import Slideshow from "@/components/Slideshow";
-import ProjectIndex from "@/components/ProjectIndex";
+import Index from "@/components/Index";
+import Info from "@/components/Info";
 export default {
   data() {
     return {
-      imgArr: [],
-      projectInfo: null,
-      bgSrc: "",
-      isShowUp: false,
-      arrowUpStyle: {
-        background: "url(./static/img/grab-up.png) no-repeat center"
-      }
+      slide: "slide-right",
+      showInfo: false
     };
   },
-  components: {
-    Slideshow,
-    ProjectIndex
-  },
-  created() {
-    // 请求轮播图资源
-    axios
-      .get("./static/json/slideshow.json")
-      .then(res => {
-        this.imgArr = Array.from(res.data);
-      })
-      .catch(function() {
-        console.log("error");
-      });
-    // 请求项目信息资源
-    axios
-      .get("./static/json/projectIndex.json")
-      .then(res => {
-        this.projectInfo = res.data.projectInfo;
-        this.bgSrc = res.data.bgSrc;
-      })
-      .catch(function() {
-        console.log("error");
-      });
-  },
-  mounted() {
-    $(window).on("scroll", () => {
-      let iScrollTop =
-        document.body.scrollTop || document.documentElement.scrollTop;
-      if (iScrollTop < document.documentElement.clientHeight) {
-        this.isShowUp = false;
-      } else {
-        this.isShowUp = true;
-      }
-    });
-  },
   methods: {
-    up() {
-      $(document.body)
-        .add(document.documentElement)
-        .animate(
-          {
-            scrollTop: 0
-          },
-          1000
-        );
+    changeSlideDirction(sDirc) {
+      this.slide = "slide-" + sDirc;
+      this.showInfo = sDirc=='right'? true : false;
     }
+  },
+  components: {
+    Index,
+    Info
   }
 };
 </script>
@@ -78,6 +31,7 @@ export default {
 <style>
 body {
   margin: 0;
+  overflow-x: hidden;
 }
 ul {
   padding: 0;
@@ -91,47 +45,24 @@ li {
   display: block;
   clear: both;
 }
-#slideshow {
-  width: 100%;
-  height: 100vh;
+.slide-left-enter,
+.slide-right-leave-to {
+  transform: translate(100%, 0);
 }
-.arrow-up {
-  cursor: pointer;
-  width: 40px;
-  height: 40px;
-  position: fixed;
-  bottom: 65px;
-  right: 5%;
-  animation: vertical 2.5s ease infinite;
-  z-index: 10;
+.slide-left-enter-to,
+.slide-left-leave,
+.slide-right-enter-to,
+.slide-right-leave {
+  transform: translate(0, 0);
 }
-@keyframes vertical {
-  30% {
-    bottom: 65px;
-  }
-  40% {
-    bottom: 55px;
-  }
-  50% {
-    bottom: 65px;
-  }
-  60% {
-    bottom: 55px;
-  }
-  70% {
-    bottom: 65px;
-  }
+.slide-left-enter-active,
+.slide-left-leave-active,
+.slide-right-enter-active,
+.slide-right-leave-active {
+  transition: transform 0.5s ease-in;
 }
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-to,
-.fade-leave {
-  opacity: 1;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
+.slide-left-leave-to,
+.slide-right-enter {
+  transform: translate(-100%, 0);
 }
 </style>
